020d556f0ac883c4744cebb502161b435ceedacc
rueiwang <want2813@gmail.com>
2023-08-24 17:52:16 +0800

    doc: add import/order rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,31 @@ module.exports = {
       extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier']
     }
   ],
-  rules: {}
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'], // node builtin module → external package
+          ['internal'], // → internal module
+          ['index', 'sibling', 'parent', 'object', 'type'] // → sort by relative path: index → ./ → ../ → object-import → type import
+        ],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before'
+          }
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['react'], // react 開頭都排第一個
+        alphabetize: {
+          order:
+            'asc' /* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */,
+          caseInsensitive: true /* ignore case. Options: [true, false] */
+        },
+        warnOnUnassignedImports: true
+      }
+    ]
+  }
 }
