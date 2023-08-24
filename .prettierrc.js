ed21f2c168f866330bd6389b94aee58ce7f4ca1f
rueiwang <want2813@gmail.com>
2023-08-24 11:38:38 +0800

    doc: prettier setting

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
+
+module.exports = config
