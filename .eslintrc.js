4f95ce816acafe52a1d215164d3d14ee3b140035
rueiwang <want2813@gmail.com>
2023-08-28 14:10:33 +0800

    fix: custom rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,56 @@
+const customRules = {
+  'func-style': ['error', 'declaration'],
+  'import/order': [
+    'error',
+    {
+      groups: [
+        ['builtin', 'external'], // node builtin module → external package
+        ['internal'], // → internal module
+        ['sibling', 'parent', 'object', 'type', 'index'], // → sort by relative path: ./ → ../ → object-import → type import → index
+      ],
+      pathGroups: [
+        {
+          pattern: 'react*/**',
+          group: 'external',
+          position: 'before',
+          patternOptions: { partial: true, nocomment: true },
+        },
+        {
+          // "import './style.css'" need to be imported at the end of the imports list
+          pattern: '*.+(css|less|scss|sass|pcss|styl)',
+          group: 'index',
+          patternOptions: { matchBase: true },
+          position: 'after',
+        },
+      ],
+      'newlines-between': 'always',
+      pathGroupsExcludedImportTypes: ['react*/**'], // react 開頭都排第一個
+      alphabetize: {
+        order:
+          'asc' /* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */,
+        caseInsensitive: true /* ignore case. Options: [true, false] */,
+      },
+      warnOnUnassignedImports: true,
+    },
+  ],
+  'hooks/sort': [
+    2,
+    {
+      groups: [
+        'useState',
+        'useRef',
+        'useSelector',
+        'useDispatch',
+        'useReducer',
+        'useContext',
+        'useCallback',
+        'useMemo',
+        'useEffect',
+      ],
+    },
+  ],
+};
+
 module.exports = {
   root: true,
   env: {
@@ -14,58 +67,8 @@ module.exports = {
         tsconfigRootDir: __dirname,
       },
       extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier'],
+      rules: { ...customRules },
     },
   ],
-  rules: {
-    'func-style': ['error', 'declaration'],
-    'import/order': [
-      'error',
-      {
-        groups: [
-          ['builtin', 'external'], // node builtin module → external package
-          ['internal'], // → internal module
-          ['index', 'sibling', 'parent', 'object', 'type'], // → sort by relative path: index → ./ → ../ → object-import → type import
-        ],
-        pathGroups: [
-          {
-            pattern: 'react*/**',
-            group: 'external',
-            position: 'before',
-            patternOptions: { partial: true, nocomment: true },
-          },
-          {
-            // "import './style.css'" need to be imported at the end of the imports list
-            pattern: '*.+(css|less|scss|sass|pcss|styl)',
-            group: 'type',
-            patternOptions: { matchBase: true },
-            position: 'after',
-          },
-        ],
-        'newlines-between': 'always',
-        pathGroupsExcludedImportTypes: ['react*/**'], // react 開頭都排第一個
-        alphabetize: {
-          order:
-            'asc' /* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */,
-          caseInsensitive: true /* ignore case. Options: [true, false] */,
-        },
-        warnOnUnassignedImports: true,
-      },
-    ],
-    'hooks/sort': [
-      2,
-      {
-        groups: [
-          'useState',
-          'useRef',
-          'useSelector',
-          'useDispatch',
-          'useReducer',
-          'useContext',
-          'useCallback',
-          'useMemo',
-          'useEffect',
-        ],
-      },
-    ],
-  },
+  rules: { ...customRules },
 };
